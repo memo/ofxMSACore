1eee6645a69f9a4b19c5878bbea36479eff0c13d
Memo Akten <memo@memo.tv>
2019-02-01 04:50:21 +0300

    addon_config

---
+++ addon_config.mk
@@ -0,0 +1,11 @@
+meta:
+	ADDON_NAME = ofxMSACore
+	ADDON_DESCRIPTION = Core wrappers for ofxMSA addons
+	ADDON_AUTHOR = Memo Akten, www.memo.tv
+	ADDON_TAGS = ""
+	ADDON_URL = https://github.com/memo/ofxMSACore
+
+common:
+	# dependencies with other addons, a list of them separated by spaces
+	# or use += in several lines
+	ADDON_DEPENDENCIES =
